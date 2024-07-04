3a554b6ccab9ec856b0b39e070fea2760b198608
Jal <106935663+JAL117@users.noreply.github.com>
2024-07-04 08:59:56 -0600

    Frontend

---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LayoutHome from "./Pages/Layouts/LayoutHome";
+import LayoutPanel from "./Pages/Layouts/LayoutPanel";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+import Panel from "./Pages/Panel";
+
+const router = createBrowserRouter([
+  {
+    path: "/AreaCliente",
+    element: <LayoutPanel />,
+    children: [
+      {
+        index: true,
+        element: <Panel />,
+      },
+      {
+        path: "Graficas",
+        element: <></>,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <LayoutHome />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/Login",
+    element: <Login />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
