220ad24a6f3e51454e026683e894fd6aee4a3490
Jal <106935663+JAL117@users.noreply.github.com>
2024-07-04 23:15:39 -0600

    Area de recuperacion de contraseña

---
+++ src/Components/RecuperacionDeContraseña/FooterR.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../../Img/LogotipoPiWeb.jpeg";
+
+const FooterR = () => {
+  return (
+    <footer className="text-white mt-5 p-4 text-center fixed-bottom" style={{ backgroundColor: "#00126E"}}>
+      <Container>
+        <Row>
+          <Col md={3} className="text-left">
+            <img src={logo} alt="Company Logo" style={{ maxWidth: "100px", borderRadius: "50%" }} />
+            <p>Contacto: +52 9671941293</p>
+            <p>Email: piwebsoft@gmail.com</p>
+          </Col>
+          <Col md={6} className="text-right mt-5">
+            <p>&copy; {new Date().getFullYear()} Piweb. Todos los derechos reservados.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default FooterR;
